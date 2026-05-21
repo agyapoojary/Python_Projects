e62e2fa6caff0227bfd9115e01f8ee8e05af9244
agyapoojary <agyaupoojary@gmail.com>
2026-05-21 14:20:41 +0530

    added files to the folder

---
+++ index/Python_Theory/To_perform_basic_set_operations.py
@@ -0,0 +1,7 @@
+set1={10,20,30,40}
+set2={30,40,50,60}
+print("union",set1.union(set2) )
+print("intersection",set1.intersection(set2) )
+print("difference",set1.difference(set2) )
+set1.add(70)
+print("updated set:",set1)
